e50c64c1b847eb511007d5ac0b6f350d694bda7c
GuidoMagni <guibonmagni@gmail.com>
2026-05-23 23:04:43 +0200

    update

--- ex1/ft_raise_exception.py
+++ ex1/ft_raise_exception.py
@@ -1,6 +1,10 @@
 def input_temperature(temp_str):
     try:
         temp = int(temp_str)
+        if temp < 0:
+            raise ValueError(f"{temp}°C is too cold for plants (min 0°C)")
+        if temp > 40:
+            raise ValueError(f"{temp}°C is too hot for plants (max 40°C)")
         return temp
     except ValueError as a:
         print(f"Caught input_temperature error: {a}")
@@ -8,13 +12,23 @@ def input_temperature(temp_str):
 
 
 def test_temperature():
-    print("=== Garden Temperature ===")
+    print("=== Garden Temperature Checker ===")
     temp_str = "25"
     print(f"\nInput data is '{temp_str}'")
     temp = input_temperature(temp_str)
     if temp is not None:
         print(f"Temperature is now {temp}°C")
     temp_str = "abc"
+    print(f"\nInput data is '{temp_str}'")
+    temp = input_temperature(temp_str)
+    if temp is not None:
+        print(f"Temperature is now {temp}°C")
+    temp_str = "100"
+    print(f"\nInput data is '{temp_str}'")
+    temp = input_temperature(temp_str)
+    if temp is not None:
+        print(f"Temperature is now {temp}°C")
+    temp_str = -50
     print(f"\nInput data is '{temp_str}'")
     temp = input_temperature(temp_str)
     if temp is not None:
---
+++ ex2/ft_different_errors.py
@@ -0,0 +1,13 @@
+def garden_operations(operation_number):
+    if operation_number == 0:
+        raise ValueError("0")
+    return(operation_number)
+
+
+def test_error_types():
+    print("=== Garden Error Types Demo ===")
+
+
+
+if __name__ == "__main__":
+    garden_operations(0)
